251d71add0327e1f13783e4c762e0be0e7519452
Rohan Arora <mac@192.168.1.7>
2022-12-19 21:52:41 +0530

    removed instances written by the user and added application context in order to manage all the beans

--- src/main/java/com/spring/basics/springbasics/BinarySeachImpl.java
+++ src/main/java/com/spring/basics/springbasics/BinarySeachImpl.java
@@ -1,16 +1,18 @@
 package com.spring.basics.springbasics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySeachImpl {
 	
+	@Autowired
 	SortAlgorithm sortAlgorithm;
 	
-	public BinarySeachImpl(SortAlgorithm sortAlgorithm) {
-		// TODO Auto-generated constructor stub
-		this.sortAlgorithm=sortAlgorithm;
-	}
+//	public BinarySeachImpl(SortAlgorithm sortAlgorithm) {
+//		// TODO Auto-generated constructor stub
+//		this.sortAlgorithm=sortAlgorithm;
+//	}
 	
 	public int binarySearch(int[] numbers, int search) {
 		
--- src/main/java/com/spring/basics/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/spring/basics/springbasics/SpringBasicsApplication.java
@@ -2,6 +2,7 @@ package com.spring.basics.springbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
@@ -9,14 +10,23 @@ public class SpringBasicsApplication {
 	// THINGS REQUIRED BY THE SPRING:
 	// 1. what are the beans required to manage by the spring
 	// 2. what are the dependencies required by the beans
-	// 3. where to search for the beans
+	// 3. where to search for the beans: This is managed by the spring boot application itself
+	// since beans and the spring base application java file are in same package. So, no need to do anything.
 
 	public static void main(String[] args) {
-		BinarySeachImpl binarySearch = new BinarySeachImpl(new QuickSortAlgorithm());
+		// BinarySeachImpl binarySearch = new BinarySeachImpl(new QuickSortAlgorithm());
+		
+		// Since Spring is managing beans, therefore, we need not to explicitely instantiate BinarySearchImpl beans.
+		// APPLICATION CONTEXT manages all the beans create by the spring framework.
+		// therefore, we are not creating instances by ourselves but spring is doing it for us.
+		
+		
+		ApplicationContext applicationContext = SpringApplication.run(SpringBasicsApplication.class, args);
+		BinarySeachImpl binarySearch = applicationContext.getBean(BinarySeachImpl.class);
+		
 		int result=binarySearch.binarySearch(new int[] {1, 2, 3}, 3);
 		System.out.print(result);
 		
-//		SpringApplication.run(SpringBasicsApplication.class, args);
 	}
 
 }
