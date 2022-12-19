1af2cb684e69cd50232ab2e768e5e0169017a61d
Rohan Arora <mac@192.168.1.7>
2022-12-19 21:34:35 +0530

    Added beans using @Component annotation as beans are instances

--- src/main/java/com/spring/basics/springbasics/BinarySeachImpl.java
+++ src/main/java/com/spring/basics/springbasics/BinarySeachImpl.java
@@ -1,5 +1,8 @@
 package com.spring.basics.springbasics;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySeachImpl {
 	
 	SortAlgorithm sortAlgorithm;
--- src/main/java/com/spring/basics/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/spring/basics/springbasics/SpringBasicsApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
+	
+	// THINGS REQUIRED BY THE SPRING:
+	//what are the beans required to manage by the spring
+	// what are the dependencies required by the beans
+	// where to search for the beans
 
 	public static void main(String[] args) {
 		BinarySeachImpl binarySearch = new BinarySeachImpl(new QuickSortAlgorithm());
